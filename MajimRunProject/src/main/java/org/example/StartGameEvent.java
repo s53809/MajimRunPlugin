f505d5a2050c6356265e5aa8b17402d90ad53559
super05hero <84126302+super05hero@users.noreply.github.com>
2024-02-05 23:38:02 +0900

    StartGameEvent

--- MajimRunProject/src/main/java/org/example/StartGameEvent.java
+++ MajimRunProject/src/main/java/org/example/StartGameEvent.java
@@ -1,2 +1,30 @@
-package org.example;public class StartGameEvent {
+package org.example;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class StartGameEvent extends Event implements Cancellable {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+    private boolean isCancelled;
+
+    @Override
+    public boolean isCancelled() {
+        return this.isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.isCancelled = cancel;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
 }
