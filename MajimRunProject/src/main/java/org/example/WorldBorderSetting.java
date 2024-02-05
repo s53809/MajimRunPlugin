c440260da05e71341bcd01ed6b49bac7b4cbe8cc
Minsoda <dg4res@gmail.com>
2024-02-05 23:50:26 +0900

    WorldBorderSetting

--- MajimRunProject/src/main/java/org/example/WorldBorderSetting.java
+++ MajimRunProject/src/main/java/org/example/WorldBorderSetting.java
@@ -1,2 +1,17 @@
-package org.example;public class WorldBorderSetting {
+package org.example;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+public class WorldBorderSetting implements Listener{
+
+    @EventHandler
+    public void WorldBorderSetting(StartGameEvent event){
+
+    }
 }
