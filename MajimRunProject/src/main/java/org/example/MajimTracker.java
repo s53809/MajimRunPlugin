1d28ed8b03aacb9113380ce41361de528a5295c5
Minsoda <dg4res@gmail.com>
2024-02-20 23:44:56 +0900

    텔포, 죽으면 불사의 토탬 셋팅 완료, 게임 종료 이벤트 함수 제작

---
+++ MajimRunProject/src/main/java/org/example/MajimTracker.java
@@ -0,0 +1,71 @@
+package org.example;
+import org.bukkit.Bukkit;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityResurrectEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import java.util.List;
+
+public class MajimTracker implements Listener {
+
+    Player majim;
+    boolean isGameStart = false;
+    @EventHandler
+    public void OnWorldBorderEvent(StartGameEvent event){
+        Bukkit.broadcastMessage("MajimTracker 실행 완료");
+        majim = MajimHandler.Ins().GetMajim();
+        Bukkit.broadcastMessage("Majim is " + majim.getName());
+        isGameStart = true;
+
+        List<Player> players;
+        players = (List<Player>)Bukkit.getOnlinePlayers();
+        for(int i = 0;i<players.size();i++){
+            Location loca = players.get(i).getLocation();
+            loca.setWorld(players.get(i).getWorld());
+            loca.setX(MajimRunner.Ins().mWorldBorderCenterX +
+                    (((MajimRunner.Ins().mWorldBorderSize / 2) - 2)
+                            * Math.cos((360 / players.size()) * i)));
+            loca.setY(10);
+            loca.setZ(MajimRunner.Ins().mWorldBorderCenterZ +
+                    (((MajimRunner.Ins().mWorldBorderSize / 2) - 2)
+                            * Math.sin((360 / players.size()) * i)));
+            loca.setY(players.get(i).getWorld().getHighestBlockAt((int)loca.getX(), (int)loca.getZ()).getY() + 2);
+            loca.setY(loca.getY() + 3);
+
+            players.get(i).teleport(loca);
+        }
+    }
+
+    @EventHandler
+    public void PlayerWalkEvent(PlayerMoveEvent event){
+        if(!isGameStart) return;
+        
+        Title.sendActionBarAll("마짐의 좌표 : " +
+                (int)majim.getLocation().getX() + " " +
+                (int)majim.getLocation().getY() + " " +
+                (int)majim.getLocation().getZ());
+    }
+
+    @EventHandler
+    public void PlayerDeadEvent(EntityResurrectEvent event){
+        if(event.getEntityType() == EntityType.PLAYER){
+            Player player = (Player)event.getEntity();
+            if(player == MajimHandler.Ins().GetMajim()){
+                EndGameEvent ev = new EndGameEvent();
+
+                Bukkit.getPluginManager().callEvent(ev);
+                if(!ev.isCancelled()){
+                    ev.setCancelled(true);
+                }
+            }
+            event.setCancelled(false);
+            player.playEffect(EntityEffect.TOTEM_RESURRECT);
+        }
+    }
+}
---
+++ MajimRunProject/src/main/java/org/example/Title.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class Title {
+
+    public static void sendActionBar(Player player, String text){
+        Bukkit.broadcastMessage(player.getName() + "에게 메세지를 보냅니다.");
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(text));
+    }
+
+    public static void sendActionBar(List<Player> players, String text){
+        for(Player player : players){
+            player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(text));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void sendActionBarAll(String text){
+        sendActionBar((List<Player>) Bukkit.getOnlinePlayers(), text);
+    }
+}
