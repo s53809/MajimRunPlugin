7bf894f3d6449bfc2c2a532756d51e23fb2c7b84
Minsoda <dg4res@gmail.com>
2024-02-22 20:51:30 +0900

    qwer

--- MajimRunProject/src/main/java/org/example/CommandMinseo.java
+++ MajimRunProject/src/main/java/org/example/CommandMinseo.java
@@ -6,13 +6,17 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class CommandMinseo implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         commandSender.sendMessage("You Find Secret Command!");
-        Bukkit.getPlayer(commandSender.getName()).setWalkSpeed(0.2f);
+        Bukkit.getPlayer(commandSender.getName()).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20 * 60, 249));
+        Bukkit.getPlayer(commandSender.getName()).addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 20 * 60, 249));
         return true;
     }
 }
